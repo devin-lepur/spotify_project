ca94c21781bbf409facf72737df0959216573c13
devin-lepur <73671652+devin-lepur@users.noreply.github.com>
2023-10-14 20:26:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var redirect_uri = "https://devin-lepur.github.io/spotify_project/";
 
 
-var client_id = "";
-var client_secret = "";
+var client_id = "47911132bbe548e283bd3c2e5c47eae9";
+var client_secret = "1a8b64b81e864fe7bab94449a862b778";
+
+var access_token = null;
+var refresh_token = null;
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
 const TOKEN = "https://accounts.spotify.com/api/token";
