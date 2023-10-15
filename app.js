493eb1b9d3d5ba2811e6ff5d7412acded0922d2f
devin-lepur <73671652+devin-lepur@users.noreply.github.com>
2023-10-14 19:45:54 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,76 +9,34 @@ const TOKEN = "https://accounts.spotify.com/api/token";
 
 
 function OnPageLoad() {
-    client_id = sessionStorage.getItem("client_id");
-    client_secret = sessionStorage.getItem("client_secret");
-
-
-    if(window.location.search.length > 0) {
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
+    if ( window.location.search.length > 0 ){
         handleRedirect();
     }
 }
 
-function handleRedirect() {
+function handleRedirect(){
     let code = getCode();
-    fetchAccessToken(code);
-    window.history.pushState("", "", redirect_uri);
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
 }
 
-function fetchAccessToken(code) {
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code;
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&cliend_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
-}
-
-function callAuthorizationApi(body) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", TOKEN, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Baisc' + btoa(client_id + ":" + client_secret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-}
-
-function handleAuthorizationResponse() {
-    if(this.status ==200) {
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-        var data = JSON.parse(this.responseText);
-        if(data.access_token != undefined) {
-            access_token = data.access_token;
-            sessionStorage.setItem("access_token", access_token);
-        }
-        if(data.refresh_token != undefined)  {
-            refresh_token = data.refresh_token;
-            sessionStorage.setItem("refresh_token", refresh_token);
-        }
-        OnPageLoad();
-    } else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-}
-
-
-
-function getCode() {
+function getCode(){
     let code = null;
     const queryString = window.location.search;
-    if(queryString.length > 0) {
+    if ( queryString.length > 0 ){
         const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get("code");
+        code = urlParams.get('code')
     }
     return code;
 }
 
-function requestAuthorization() {
+function requestAuthorization(){
     client_id = document.getElementById("clientId").value;
     client_secret = document.getElementById("clientSecret").value;
-    sessionStorage.setItem("client_id", client_id);
-    sessionStorage.setItem("client_secret", client_secret);
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
 
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
@@ -87,4 +45,51 @@ function requestAuthorization() {
     url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
     window.location.href = url; // Show Spotify's authorization screen
+}
+
+function fetchAccessToken( code ){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function refreshAccessToken(){
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
 }
