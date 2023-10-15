442d83cab486ffc488e27673df91f20d9491db43
devin-lepur <73671652+devin-lepur@users.noreply.github.com>
2023-10-14 18:44:14 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,84 @@
-var redirect_uri = "https://devin-lepur.github.io/spotify_project/";
+var redirect_uri = "C:\Users\Devin\Documents\Projects\spotify_project\index.html";
 
 
 var client_id = "";
 var client_secret = "";
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
+
 
 function OnPageLoad() {
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
 
+
+    if(window.location.search.length > 0) {
+        handleRedirect();
+    }
 }
 
+function handleRedirect() {
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirect_uri);
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&cliend_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Baisc' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse() {
+    if(this.status ==200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if(data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if(data.refresh_token != undefined)  {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        OnPageLoad();
+    } else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+
+
+function getCode() {
+    let code = null;
+    const queryString = window.location.search;
+    if(queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get("code");
+    }
+    return code;
+}
 
 function requestAuthorization() {
     client_id = document.getElementById("clientId").value;
     client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret);
 
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
